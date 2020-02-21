d04819edbefaced074de3060e92f9a8c03173e20
Max <Max@192.168.2.23>
2020-02-21 16:06:55 -0500

    change password

--- WebServicesCPL/src/java/cpl/Cpl.java
+++ WebServicesCPL/src/java/cpl/Cpl.java
@@ -5,6 +5,15 @@
  */
 package cpl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
@@ -13,6 +22,8 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import net.sf.json.JSONObject;
 
 /**
  * REST Web Service
@@ -35,20 +46,71 @@ public class Cpl {
      * Retrieves representation of an instance of cpl.Cpl
      * @return an instance of java.lang.String
      */
-    @GET
-    @Produces("application/xml")
-    public String getXml() {
+     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("changePassword&{password}&{userName}&{dob}")
+    public String InsertTeam(@PathParam("password") String password,
+            @PathParam("userName") String userName,@PathParam("dob") String dob)
+                      {
         //TODO return proper representation object
-        throw new UnsupportedOperationException();
+        String msg;
+        Connection conn = null;
+        JSONObject firstObject = new JSONObject();
+        Date cd = new Date();
+        long epoch = cd.getTime();
+        int ts = (int) (epoch / 1000);
+        System.out.println("Current Time Stamp: " + ts);
+        PreparedStatement stmt = null;
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://198.71.227.97:3306/cpl", "mahesh", "eQa2j#78");
+            //System.out.println(conn)
+            String sql;
+            sql = "update User set password=? where userName=? && dob=?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, password);
+            stmt.setString(2, userName);
+            stmt.setString(3,dob);
+            int rs = stmt.executeUpdate();
+            firstObject.accumulate("Status", "Ok");
+            firstObject.accumulate("TimeStamp ", ts);
+             
+            SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+            Date parsed = null;
+            try {
+                parsed = format.parse(dob);
+            } catch (ParseException ex) {
+                Logger.getLogger(Cpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            java.sql.Date sqlDate = new java.sql.Date(parsed.getTime());
+
+
+            if (rs > 0) {
+                msg = rs + " Record have successfully been updated.";
+                firstObject.accumulate("message : ", msg);
+            } else {
+                msg = rs + " No record updated.";
+                firstObject.accumulate("message : ", msg);
+            }
+
+        } catch (Exception ex) {
+            msg = ex.getMessage();
+            firstObject.accumulate("Status", "Error");
+            firstObject.accumulate("TimeStamp ", ts);
+            firstObject.accumulate("Message :", msg);
+
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Cpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+        return firstObject.toString();
     }
 
-    /**
-     * PUT method for updating or creating an instance of Cpl
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-    @PUT
-    @Consumes("application/xml")
-    public void putXml(String content) {
-    }
 }
