b4c4c1068327632afeaf96d753684f819454f89d
Mandeep Kaur <kaurm070795@gmail.com>
2020-02-22 23:04:23 -0500

    Created viewFeedback method

--- WebServicesCPL/src/java/cpl/Cpl.java
+++ WebServicesCPL/src/java/cpl/Cpl.java
@@ -371,5 +371,80 @@ public class Cpl {
 
         return firstObject.toString();
     }
+    
+       @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("viewFeedback")
+    public String ViewFeedback() {
+
+        Connection con = null;
+        PreparedStatement stm = null;
+        String sql = null;
+        ResultSet rs;
+        String result = null;
+        JSONObject jsonObject = new JSONObject();
+        String status = "OK";
+        String message = null;
+        JSONObject mainObject=new JSONObject();
+        JSONArray list = new JSONArray();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://198.71.227.97:3306/cpl", "mahesh", "eQa2j#78");
+
+            sql = "Select * from Feedback";
+            stm= con.prepareStatement(sql);
+            rs = stm.executeQuery();
+            
+          
+            while (rs.next()) {
+
+                int feedbackId = rs.getInt("feedbackId");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                String email = rs.getString("email");
+              
+              
+                mainObject.accumulate("feedbackId", feedbackId);
+                mainObject.accumulate("title", title);
+                mainObject.accumulate("description", description);
+                mainObject.accumulate("email", email);
+                System.out.println(mainObject);
+                list.add(mainObject);
+                mainObject.clear(); 
+
+            }
+
+        } catch (SQLException ex) {
+             Logger.getLogger(Feedback.class.getName()).log(Level.SEVERE, null, ex);
+             System.out.println(ex);
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Feedback.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+     
+            jsonObject.accumulate("Status", status);
+            jsonObject.accumulate("Message", result);
+            jsonObject.accumulate("String", list);
+           
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Feedback.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (stm != null) {
+                    try {
+                        stm.close();
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Feedback.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+
+        return jsonObject.toString();
+    }    
+    
 
 }
